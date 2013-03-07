7ad7c1d1c0a0ea361efca9af3d9aa84a80248aba
Jo Somers <sayhello@josomers.be>
2013-03-07 01:05:13 +0100

    added a hello world application with maven

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+Change Log
+==========
+
+*No releases yet...*
---
+++ application/test/com/webcomrades/demo/test/MyFirstTest.java
@@ -0,0 +1,12 @@
+package com.webcomrades.demo.test;
+
+import android.test.AndroidTestCase;
+
+public class MyFirstTest extends AndroidTestCase {
+
+	public void doSomeTest() {
+		boolean init = true;
+		assertEquals(true, init);
+	}
+	
+}
